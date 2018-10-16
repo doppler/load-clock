659b318fb44c313f3452ef8c074893e8fd73c068
David Rose <doppler@gmail.com>
2018-10-16 11:50:52 -0400

    Make LoadTimer look somewhat uh... nice?

--- src/LoadTimer.js
+++ src/LoadTimer.js
@@ -4,7 +4,7 @@ import './LoadTimer.css'
 const LoadTimer = ({load, time, slots, backgroundColor, foregroundColor}) =>
   <div className="LoadTimer" style={{backgroundColor: backgroundColor, color: foregroundColor}}>
     <div className="title">{load}</div>
-    <div className="text">{time}</div>
+    <div className="time">{time}</div>
     <div className="slots">Slots Remaining: {slots}</div>
   </div>
 
