04a52b0a402591d4ce5bc97d22722b64027de3e9
David Rose <doppler@gmail.com>
2018-10-16 13:34:31 -0400

    Add WindInfo with direction

--- src/WeatherSection.js
+++ src/WeatherSection.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import WindInfo from './WindInfo'
 import Compass from './Compass'
 import WeatherSummary from './WeatherSummary'
 import './WeatherSection.css'
 
 const WeatherSection = ({winds, weather, loadsFlownToday}) =>
   <div className="WeatherSection">
+    <WindInfo winds={winds}/>
     <Compass winds={winds}/>
     <WeatherSummary weather={weather} winds={winds} loadsFlownToday={loadsFlownToday} />
   </div>
---
+++ src/WindInfo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './WindInfo.css';
+
+const directionStringFromDirection = (direction) => {
+  const dirs = [
+    'N', 'NNE', 'NE', 'ENE',
+    'E', 'ESE', 'SE', 'SSE',
+    'S', 'SSW', 'SW', 'WSW',
+    'W', 'WNW', 'NW', 'NNW'
+  ];
+  const directionModulo = direction %= 360;
+  const index = Math.round(((directionModulo) < 0 ? direction + 360 : direction) / 22.5) % 16;
+  return dirs[index];
+}
+
+const WindInfo = ({winds}) => {
+  let directionString = directionStringFromDirection(winds.direction);
+  return (
+    <div className="WindInfo">
+      {directionString} ( {winds.direction} &deg; )
+    </div>
+  )
+}
+
+export default WindInfo;
