61396656f79d96e80073269dd991ef9bf5ed9ff9
David Rose <doppler@gmail.com>
2018-10-16 21:12:35 -0400

    Remove WindInfo

--- src/WeatherSection.js
+++ src/WeatherSection.js
@@ -1,13 +1,10 @@
 import React from 'react';
-// import WindInfo from './WindInfo'
 import Compass from './Compass'
 import Summary from './Summary'
-// import './WeatherSection.css'
 
 const WeatherSection = ({winds, prevWindDirections, weather, loadsFlownToday}) =>
   <div className="WeatherSection">
     <Compass winds={winds} prevWindDirections={prevWindDirections}/>
-    {/* <WindInfo winds={winds}/> */}
     <Summary weather={weather} winds={winds} loadsFlownToday={loadsFlownToday} />
   </div>
 
