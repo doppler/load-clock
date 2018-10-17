522e4ca9a6095f6f52ffd7373b0117879ec93645
David Rose <doppler@gmail.com>
2018-10-16 21:38:02 -0400

    Clean up LoadTimer

--- src/LoadTimer.js
+++ src/LoadTimer.js
@@ -3,9 +3,11 @@ import React from 'react'
 
 const LoadTimer = ({load, time, slots, backgroundColor, foregroundColor}) =>
   <div className="LoadTimer" style={{backgroundColor: backgroundColor, color: foregroundColor}}>
-    <div className="title">{load}</div>
+    <div className="header">
+      <div className="title">{load}</div>
+      <div className="slots">Slots Remaining: {slots}</div>
+    </div>
     <div className="time">{time}</div>
-    <div className="slots">Slots Remaining: {slots}</div>
   </div>
 
 export default LoadTimer;
